046ac120892ece0ce13968f84ebb5a646a090e7d
valentincgd <hypetofarstreet@gmail.com>
2022-10-20 16:55:47 +0200

    Update login

--- app.py
+++ app.py
@@ -1,31 +1,41 @@
-from flask import Flask , redirect , render_template
+from flask import Flask, render_template_string,render_template, request, session, redirect, url_for
 
 
 
 app=Flask(__name__)
 
 
+
+app.secret_key = 'da0VyW270O7802KINXaHtcmO3bufcMtW'
+
 @app.route('/',methods=['GET'])
 def get_home():
     return render_template('home.html')
 
 
-@app.route('/login',methods=['GET'])
+@app.route('/login',methods=['GET','POST'])
 def get_login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        print(session['Session'])
+        return render_template('login.html')
+    if request.method == 'POST':
+            return redirect('/')
 
 
-@app.route('/signup',methods=['GET'])
+
+@app.route('/signup',methods=['GET','POST'])
 def get_signup():
-    return render_template('signup.html')
+    if request.method == 'GET':
+        return render_template('signup.html')
+    if request.method == 'POST':
+        session['Session'] = request.form['username']
 
-@app.route('/login',methods=['POST'])
-def login_post():
-    return redirect('/')
+        
+        #ADD DANS LA BDD
 
-@app.route('/signup',methods=['POST'])
-def signup_post():
-    return redirect('/')
+        return redirect('/')
+
+
 
 @app.route('/logout',methods=['GET'])
 def logout():
