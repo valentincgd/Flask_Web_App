bb2ce3ba9b59072056efcca0f5a9fa6128b6818b
BAYET Paul <paulpierrebayet@gmail.com>
2022-10-21 14:17:04 +0200

    design

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,6 @@
+inputUser = document.getElementById('username-txt');
+labelUser = document.getElementById('username-label');
+
 inputEmail = document.getElementById('email-txt');
 labelEmail = document.getElementById('email-label');
 
@@ -15,5 +18,12 @@ inputPass.addEventListener("keyup", (event) => {
     labelPass.setAttribute('class', '');
     if(inputPass.value != ""){
         labelPass.setAttribute('class', 'focus');
+    }
+});
+
+inputUser.addEventListener("keyup", (event) => {
+    labelUser.setAttribute('class', '');
+    if(inputUser.value != ""){
+        labelUser.setAttribute('class', 'focus');
     }
 });
